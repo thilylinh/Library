191c6a5e7ae6d84175b6023fc070190f89753c04
Ly Linh <thilylinh98@gmail.com>
2020-12-03 16:30:51 +0700

    add home page

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext _dataContext;
+        public HomeController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public IActionResult Index()
         {
+            //count books được mượn trong tháng
+
+            //count số lượt mượn sách trong tháng
+            ViewBag.SoLuotMuon = _dataContext.MuonTras.Where(m => m.NgayMuon.Month == DateTime.Now.Month).Count();
+            //count 
             return View();
         }
     }
